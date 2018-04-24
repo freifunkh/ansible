aa9973b1bed12903a6a6645df801d14a0ea4399b
lemoer <git@irrelefant.net>
2018-04-24 21:35:13 +0200

    node_exporter: add export_bpfcountd.sh script, which was missing before
    Sorry, forgot to commit it.
    
    Fixes 7d5e41af61eb42bc591259d7b9f444803060ff32.

---
+++ roles/node_exporter/templates/export_bpfcountd.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+for usock_path in /var/run/bpfcountd.*.sock; do
+
+	stats=$(nc -w 1 -U "$usock_path")
+	iface=$(echo "$usock_path" | awk -F. '{print $(NF-1)}')
+
+	for line in $stats; do
+		identifier=$(echo "$line" | cut -d ":" -f 1)
+		bytes=$(echo "$line" | cut -d ":" -f 2)
+		packets=$(echo "$line" | cut -d ":" -f 3)
+
+		echo "bpfcountd_packets{iface=\"$iface\",$identifier} $packets"
+		echo "bpfcountd_bytes{iface=\"$iface\",$identifier} $bytes"
+	done
+
+done
