f1996b7812245bb8bb089ce45cca02963ed98f31
lemoer <git@irrelefant.net>
2018-04-26 01:08:24 +0200

    node_exporter: export batctl tg and batctl o length

--- roles/node_exporter/templates/export_batman.sh
+++ roles/node_exporter/templates/export_batman.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+BATCTL=/usr/sbin/batctl
+
 for batdev in /sys/class/net/bat*; do
 	test -d ${batdev} || exit 0
 	batdev=$(basename $batdev)
@@ -8,4 +11,9 @@ for batdev in /sys/class/net/bat*; do
 			print "batman_" $1 "{batdev=\"" batdev "\"} " $2
 		}
 	'
+
+	echo "batman_originator_count{batdev=\"${batdev}\",selected=\"false\"}" $($BATCTL -m ${batdev} o | egrep '^   ' | wc -l)
+	echo "batman_originator_count{batdev=\"${batdev}\",selected=\"true\"}" $($BATCTL -m ${batdev} o | egrep '^ \*' | wc -l)
+	echo "batman_tg_count{batdev=\"${batdev}\",type=\"multicast\"}" $(($($BATCTL -m ${batdev} tg -m | wc -l)-2))
+	echo "batman_tg_count{batdev=\"${batdev}\",type=\"unicast\"}" $(($($BATCTL -m ${batdev} tg -u | wc -l)-2))
 done
