5e6256fdcfc50244063f18ba98f1771b4f75b35e
Oliver Krueger <mail@oliverkrueger.de>
2018-08-19 02:10:35 +0200

    node_exporter: fix for long running scripts

--- roles/node_exporter/templates/run_exporters.sh
+++ roles/node_exporter/templates/run_exporters.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-rm -f /var/run/node_txtfile/*.prom
-
 for f in /opt/node_exporter/*; do
   bn=$(basename ${f%.sh})
-  ${f} > /var/run/node_txtfile/${bn}.prom
+  ${f} > /var/run/node_txtfile/${bn}.tmp
+  mv /var/run/node_txtfile/${bn}.tmp /var/run/node_txtfile/${bn}.prom
 done
