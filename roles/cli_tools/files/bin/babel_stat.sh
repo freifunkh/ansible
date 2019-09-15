024a9958cac2530d03015a6fcf0cb0e547d143bc
lemoer <git@irrelefant.net>
2019-09-15 14:42:28 +0200

    cli_tools: minor improvements babel_stat.sh tool

--- roles/cli_tools/files/bin/babel_stat.sh
+++ roles/cli_tools/files/bin/babel_stat.sh
@@ -2,6 +2,15 @@
 
 data=$(echo dump | nc ::1 33123 -w 1)
 
+filter_whitespace () {
+	grep -v '^\s*$'
+}
+
+count_lines () {
+	# wc -l is not suitable as it counts empty lines
+	awk 'BEGIN{c=0}NF{c++}END{print c}'
+}
+
 d () {
 	echo -n "$data"
 }
@@ -13,13 +22,15 @@ d | grep interface
 
 ifaces=$(d | grep interface | cut -d " " -f 3)
 
-TOK_10="route 10." 
+TOK_10="route 10\\." 
+TOK_100="route 100\\." 
 TOK_2a02="route 2a02:790:ff:"
 
-xroute_10=$(d | grep "$TOK_10" | wc -l)
-xroute_2a02=$(d | grep "$TOK_2a02" | wc -l)
-xroute_rest=$(d | grep "xroute" | grep -v "$TOK_10" | grep -v "$TOK_2a02" | sed '/^$/d' )
-xroute_rest_cnt=$(echo "$xroute_rest" | wc -l)
+xroute_10=$(d | grep "$TOK_10" | count_lines)
+xroute_100=$(d | grep "$TOK_100" | count_lines)
+xroute_2a02=$(d | grep "$TOK_2a02" | count_lines)
+xroute_rest=$(d | grep "xroute" | grep -v "$TOK_10" | grep -v "$TOK_2a02" | grep -v "$TOK_100" )
+xroute_rest_cnt=$(echo "$xroute_rest" | count_lines)
 
 echo
 echo "NEIGHBOURS:"
@@ -32,6 +43,7 @@ echo "EXPORTED ROUTES:"
 echo
 
 echo "ipv4 routes beginning with 10..           : $xroute_10"
+echo "ipv4 routes beginning with 100..          : $xroute_100"
 echo "ipv6 routes beginning with 2a02:790:ff:...: $xroute_2a02"
 echo "other routes                              : $xroute_rest_cnt"
 echo
@@ -58,30 +70,33 @@ ifroutes_deact() {
 	ifroutes_total | grep "installed no"
 }
 
-if [ $(ifroutes_total | wc -l) -lt 1 ]; then
+if [ $(ifroutes_total | count_lines) -lt 1 ]; then
 continue
 fi 
 
-echo "IMPORTED ROUTES via $iface: ($(ifroutes | wc -l))"
+echo "IMPORTED ROUTES via $iface: ($(ifroutes | count_lines))"
 echo
 
-TOK_10="prefix 10." 
+TOK_10="prefix 10\\." 
+TOK_100="prefix 100\\." 
 TOK_2a02="prefix 2a02:790:ff:"
 
-xroute_10=$(ifroutes | grep "$TOK_10" | wc -l)
-xroute_2a02=$(ifroutes | grep "$TOK_2a02" | wc -l)
-xroute_rest=$(ifroutes | grep -v "$TOK_10" | grep -v "$TOK_2a02")
-xroute_rest_cnt=$(echo "$xroute_rest" | wc -l)
+xroute_10=$(ifroutes | grep "$TOK_10" | count_lines)
+xroute_100=$(ifroutes | grep "$TOK_100" | count_lines)
+xroute_2a02=$(ifroutes | grep "$TOK_2a02" | count_lines)
+xroute_rest=$(ifroutes | grep -v "$TOK_10" | grep -v "$TOK_2a02" | grep -v "$TOK_100" )
+xroute_rest_cnt=$(echo "$xroute_rest" | count_lines)
 
 fmt_route () {
 	cut -d " " -f 4-7,12-15,8,9
 }
 
-echo "inactive routes                                  : $(ifroutes_deact | wc -l)"
-echo "active routes                                    : $(ifroutes | wc -l)"
-echo "routes total                                     : $(ifroutes_total | wc -l)"
+echo "inactive routes                                  : $(ifroutes_deact | count_lines)"
+echo "active routes                                    : $(ifroutes | count_lines)"
+echo "routes total                                     : $(ifroutes_total | count_lines)"
 echo
 echo "active ipv4 routes beginning with 10..           : $xroute_10"
+echo "active ipv4 routes beginning with 100..          : $xroute_100"
 echo "active ipv6 routes beginning with 2a02:790:ff:...: $xroute_2a02"
 echo "active other routes                              : $xroute_rest_cnt"
 echo
