38458f7da5637759c68c35db334c802e29c61720
lemoer <git@irrelefant.net>
2019-12-29 17:27:55 +0100

    ffh.telegraf: readd fastd peer counter

--- roles/cli_tools/files/bin/ffh_peer_counter.sh
+++ roles/cli_tools/files/bin/ffh_peer_counter.sh
@@ -13,8 +13,25 @@ print_single() {
     nc -U /var/run/fastd.mesh_fastd_$1.sock 2>/dev/null | jq '.peers[] | select(.connection != null ) | .name | select( test("^(?!sn\\d+|harvester)"))' | wc -l;
 }
 
+print_json() {
+    total=0
+    for SOCK in $(ls -1 /var/run/fastd.mesh_fastd*.sock)
+    do
+	domain=$(nc -U ${SOCK} 2>/dev/null | jq '.peers[] | select(.connection != null ) | .name | select( test("^(?!sn\\d+|harvester)"))' | wc -l);
+        total=$(python -c "print($total+$domain)")
+    done
+
+    cat<<EOF
+[
+     { "domain": "all", "peer_count": $total }
+]
+EOF
+}
+
 case "$1" in
     -a) print_all
+      ;;
+    -j) print_json
       ;;
      *) print_single $1
       ;;
