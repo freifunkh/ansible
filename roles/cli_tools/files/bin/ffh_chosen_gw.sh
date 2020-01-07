6b9d7e2ecc979d5787ee2b7d5adb486704d6ccfb
lemoer <git@irrelefant.net>
2020-01-07 23:24:42 +0100

    telegraf: chosen gateways differ in mac a bit more than thought

--- roles/cli_tools/files/bin/ffh_chosen_gw.sh
+++ roles/cli_tools/files/bin/ffh_chosen_gw.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-# the addresses of one supernode do only differ in the last octet, so we match for the first five octets
-node_id_prefix=$(cat /sys/class/net/bat10/address | cut -d ':' -f 1-5 | tr -d ':')
-meshviewer=$(curl --compressed -s "https://harvester.ffh.zone/meshviewer.json" | jq '.nodes | map({ gateway: .gateway, gateway6: .gateway6 })')
+# the addresses of one supernode do only differ in the 4th and last octet, so we match for the four octets
+node_id_prefix=$(cat /sys/class/net/bat10/address | cut -d ':' -f 1-3 | tr -d ':')..$(cat /sys/class/net/bat10/address | cut -d ':' -f 5 | tr -d ':')
+meshviewer=$(curl --compressed -s "https://harvester.ffh.zone/meshviewer.json" | jq '.nodes | map({ gateway: .gateway, gateway6: .gateway6})')
 
 get() {
-	echo "$meshviewer" | jq 'map(select(.'$1' and (.'$1' | match("^'${node_id_prefix}'")))) | length'
+	echo "$meshviewer" | jq 'map(select(.'$1' and (.'$1' | gsub(":";"") | match("^'${node_id_prefix}'")))) | length'
 }
 
 echo \[
 echo '    { "domain": "all", "ip_version": 4, "count": '$(get gateway)'},'
 echo '    { "domain": "all", "ip_version": 6, "count": '$(get gateway6)'}'
 echo \]
-
