64601de4861a8c9dad427358a419ed15d7e605fc
CodeFetch <me@bibbl.com>
2021-01-04 15:51:59 +0100

    mesh_tunneldigger: initial commit
    This commit adds a tunneldigger instance for each domain.

--- roles/ffh.supernode/files/wait_for_iface.sh
+++ roles/ffh.supernode/files/wait_for_iface.sh
@@ -6,8 +6,10 @@ do
     sleep 1
 done
 
-while ! [ "$(cat /sys/class/net/$1/operstate)" = "up" ]
-do
-    echo  "waiting for $1 to be up"
-    sleep 1
-done
+if [$# -eq 1]
+    while ! [ "$(cat /sys/class/net/$1/operstate)" = "up" ]
+    do
+        echo  "waiting for $1 to be up"
+        sleep 1
+    done
+fi
