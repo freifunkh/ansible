ee501b54634b9876d0df02ac3d871020f09ffb84
Leonardo Mörlein <git@irrelefant.net>
2021-02-05 23:58:55 +0100

    ffh.telegraf: export kea dhcp stuff

--- roles/ffh.cli_tools/files/bin/ffh_dhcp_leases.sh
+++ roles/ffh.cli_tools/files/bin/ffh_dhcp_leases.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
 
-count_leases() {
-	echo "$leases" | grep $1 | wc -l
-}
+if [ -f /usr/sbin/dhcpd ]; then
+	# ISC DHCP SERVER (legacy)
+	count_leases() {
+		echo "$leases" | grep $1 | wc -l
+	}
 
-leases=$(grep -e "^ *binding state" /var/lib//dhcp/dhcpd.leases)
+	leases=$(grep -e "^ *binding state" /var/lib//dhcp/dhcpd.leases)
 
-echo [
-for state in free used touched defined; do
-	echo -n '     { "domain": "all", "state": "'${state}'", "count": '$(dhcpd-pools -f j | jq "[.subnets[].${state}] | add")' }'
+	echo [
+	for state in free used touched defined; do
+		echo -n '     { "domain": "all", "state": "'${state}'", "count": '$(dhcpd-pools -f j | jq "[.subnets[].${state}] | add")' }'
 
-	if [ "$state" = "defined" ]; then
-		echo 
-	else
-		echo ,
-	fi
-done
-echo ]
+		if [ "$state" = "defined" ]; then
+			echo
+		else
+			echo ,
+		fi
+	done
+	echo ]
+elif [ -f /usr/sbin/kea-dhcp4 ]; then
+	# KEA DHCP SERVER
 
+	lease_count=$(sudo /etc/zabbix/bin/kea-dhcp4-server.master.sh | \
+		jq '.arguments["cumulative-assigned-addresses"][0][0]')
+
+	echo [
+	echo '     { "domain": "all", "state": "used", "count": '"$lease_count"' }'
+	echo ]
+fi
