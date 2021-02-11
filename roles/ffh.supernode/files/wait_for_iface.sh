daf5e6fe4f83e3b193bd21638802cdeb94158dd5
aiyion.prime <git@aiyionpri.me>
2021-02-11 22:48:51 +0100

    ffh.supernode: add warning in wait_for_iface.sh
    batman 2021.0 wont set operstate anymore

--- roles/ffh.supernode/files/wait_for_iface.sh
+++ roles/ffh.supernode/files/wait_for_iface.sh
@@ -8,6 +8,12 @@ done
 
 if [ $# -eq 1 ]
 then
+    # usage of operstate is not supported since batman2021.0 anymore
+    # no need to catch this further, since 'we get batman systemd support soon'
+    if [[ $1 = bat* ]];
+    then
+        echo "Careful, this is likely not supported anymore."
+    fi
     while ! [ "$(cat /sys/class/net/$1/operstate)" = "up" ]
     do
         echo  "waiting for $1 to be up"
