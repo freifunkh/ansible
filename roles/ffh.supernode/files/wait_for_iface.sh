dcc04a996989095a7c8201f29bd4ef429e0e90df
aiyion.prime <git@aiyionpri.me>
2021-02-11 23:59:00 +0100

    wait_for_iface.sh: stay posix compliant

--- roles/ffh.supernode/files/wait_for_iface.sh
+++ roles/ffh.supernode/files/wait_for_iface.sh
@@ -10,10 +10,9 @@ if [ $# -eq 1 ]
 then
     # usage of operstate is not supported since batman2021.0 anymore
     # no need to catch this further, since 'we get batman systemd support soon'
-    if [[ $1 = bat* ]];
-    then
+    case $1 in bat*)
         echo "Careful, this is likely not supported anymore."
-    fi
+    esac
     while ! [ "$(cat /sys/class/net/$1/operstate)" = "up" ]
     do
         echo  "waiting for $1 to be up"
