416d356f745c83ca492e436cf238c4a264319bf0
lemoer <git@irrelefant.net>
2021-08-29 23:53:32 +0200

    tests: add mail.py script

---
+++ tests/mail.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import datetime
+import pytz
+import smtplib, ssl
+from email.utils import make_msgid
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email.charset import Charset, QP
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+from read_play import *
+
+SMTP_HOST = 'mail.ffh.zone'
+SMTP_PORT = 25
+SMTP_USE_STARTTLS = False
+SMTP_FROM = "me@irrelefant.net"
+SMTP_REPLY_TO_EMAIL = "me@irrelfant.net"
+
+SMTP_TO = 'me@irrelefant.net'
+
+def send_mail(subject, message, message_html, to):
+    msgid = make_msgid()
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = str(Header(subject, 'utf-8'))
+    msg['From'] = str(Header(SMTP_FROM, 'utf-8'))
+    msg['To'] = str(Header(to, 'utf-8'))
+    msg['Message-ID'] = msgid
+    msg['Reply-To'] = SMTP_REPLY_TO_EMAIL
+    msg['Date'] = datetime.datetime.now(pytz.utc).strftime("%a, %e %b %Y %T %z")
+
+    # add message
+    charset = Charset('utf-8')
+    # QP = quoted printable; this is better readable instead of base64, when
+    # the mail is read in plaintext!
+    charset.body_encoding = QP
+
+    message_part = MIMEText(message.encode('utf-8'), 'plain', charset)
+    msg.attach(message_part)
+
+    message_part2 = MIMEText(message_html.encode('utf-8'), 'html', charset)
+    msg.attach(message_part2)
+
+    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+        server.ehlo()
+        if SMTP_USE_STARTTLS:
+            context = ssl.create_default_context()
+            server.starttls(context=context)
+
+        server.sendmail(SMTP_FROM, to, msg.as_string())
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+
+    msg_txt = read_play(filename)
+    msg_html = read_play(filename, 'html')
+
+    send_mail('Daily Report of Ansible Run', msg_txt, msg_html, SMTP_TO)
--- tests/read_play.py
+++ tests/read_play.py
@@ -4,78 +4,95 @@ import sys
 import json
 import difflib
 
-CRED = '\033[31m'
-CGREEN = '\33[32m'
-CYELLOW = '\33[33m'
-CEND = '\033[0m'
+def read_play(filename, highlighting=None):
+    result = ''
+    NL = '\n'
+    start = ''
+    end = ''
 
-with open(sys.argv[1]) as f:
-    res = json.load(f)
+    if highlighting == 'html':
+        CRED = '<span style="color: red;">'
+        CGREEN = '<span style="color: green;">'
+        CYELLOW = '<span style="color: #d99400;">'
+        CEND = '</span>'
+        NL = NL
+        start = '<html><head></head><body><pre>'
+        end = '</pre></body></html>'
+    elif highlighting == 'terminal':
+        CRED = '\033[31m'
+        CGREEN = '\33[32m'
+        CYELLOW = '\33[33m'
+        CEND = '\033[0m'
+    else:
+        CRED = ''
+        CGREEN = ''
+        CYELLOW = ''
+        CEND = ''
 
+    with open(filename) as f:
+        res = json.load(f)
 
-for play in res['plays']:
-    for task in play['tasks']:
-        taskname = task['task']['name']
+    for play in res['plays']:
+        for task in play['tasks']:
+            taskname = task['task']['name']
 
-        for hostname, host in task['hosts'].items():
+            for hostname, host in task['hosts'].items():
 
-            if 'failed' in host and host['failed']:
-                print(f"[{hostname}]  {taskname}")
-                print(CRED + '############### FAILED ##################' + CEND)
-                print(host['msg'])
-                continue
+                if 'failed' in host and host['failed']:
+                    result += f"[{hostname}]  {taskname}" + "\n"
+                    result += CRED + '############### FAILED ##################' + CEND + "\n"
+                    result += host['msg'] + "\n"
+                    continue
 
-            if not host['changed']:
-                continue
+                if not host['changed']:
+                    continue
 
-            print(f"[{hostname}]  {taskname}")
+                result += f"[{hostname}]  {taskname}" + "\n"
 
-            if 'diff' not in host:
-                print(CYELLOW + 'diff: n/a\n' + CEND)
-                continue
+                if 'diff' not in host:
+                    result += CYELLOW + 'diff: n/a\n' + CEND + "\n"
+                    continue
 
-            diffs = []
-            if isinstance(host['diff'],list):
-                for diff in host['diff']:
+                diffs = []
+                if isinstance(host['diff'],list):
+                    for diff in host['diff']:
+                        before = diff.get('before', '')
+                        after = diff.get('after', '')
+
+                        after = after.splitlines(keepends=True)
+                        before = before.splitlines(keepends=True)
+
+                        if 'before_header' in diff:
+                            fromfile = diff['before_header']
+                        else:
+                            fromfile = '/dev/null'
+                        tofile = diff['after_header']
+                        diffs.extend(difflib.unified_diff(before, after,
+                            fromfile=fromfile, tofile=tofile))
+
+                    for line in diffs:
+                        if not line.endswith('\n'):
+                            line += '\n'
+
+                        if line.startswith('-'):
+                            result += CRED + line + CEND
+                        elif line.startswith('+'):
+                            result += CGREEN + line + CEND
+                        else:
+                            result += line
+
+                    result += "\n"
+                elif isinstance(host['diff'], dict):
+                    diff = host['diff']
                     before = diff.get('before', '')
                     after = diff.get('after', '')
+                    result += CRED +   '--- BEFORE: ' + str(before) + CEND + "\n"
+                    result += CGREEN + '+++  AFTER: ' + str(after) + CEND + "\n"
+                    result += "\n"
+                else:
+                    result += CRED + 'Unknown format for diff: ' + type(host['diff']) + CEND + "\n"
 
-                    after = after.splitlines(keepends=True) 
-                    before = before.splitlines(keepends=True)
+    return (start + result + end).replace('\n', NL)
 
-                    if 'before_header' in diff:
-                        fromfile = diff['before_header']
-                    else:
-                        fromfile = '/dev/null'
-                    tofile = diff['after_header']
-                    diffs.extend(difflib.unified_diff(before, after,
-                        fromfile=fromfile, tofile=tofile))
-
-                for line in diffs:
-                    if not line.endswith('\n'):
-                        line += '\n'
-
-                    if line.startswith('-'):
-                        print(CRED + line + CEND, end='')
-                    elif line.startswith('+'):
-                        print(CGREEN + line + CEND, end='')
-                    else:
-                        print(line, end='')
-
-                print()
-            elif isinstance(host['diff'], dict):
-                diff = host['diff']
-                before = diff.get('before', '')
-                after = diff.get('after', '')
-                print(CRED +   '--- BEFORE: ' + str(before) + CEND)
-                print(CGREEN + '+++  AFTER: ' + str(after) + CEND)
-                print()
-            else:
-                print(CRED + 'Unknown format for diff: ' + type(host['diff']) + CEND)
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    print(read_play(sys.argv[1], "terminal"))
