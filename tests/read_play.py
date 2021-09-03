84d4b792ddf2317f160e00274880549b45cd1111
lemoer <git@irrelefant.net>
2021-09-03 19:01:26 +0200

    tests: parse host correctly if .result exists

--- tests/read_play.py
+++ tests/read_play.py
@@ -4,6 +4,105 @@ import sys
 import json
 import difflib
 
+def parse_result(res, highlighting=None):
+    result = ''
+    NL = '\n'
+    start = ''
+    end = ''
+
+    if highlighting == 'html':
+        CRED = '<span style="color: red;">'
+        CGREEN = '<span style="color: green;">'
+        CYELLOW = '<span style="color: #d99400;">'
+        CEND = '</span>'
+        NL = NL
+        start = '<html><head></head><body><pre>'
+        end = '</pre></body></html>'
+    elif highlighting == 'terminal':
+        CRED = '\033[31m'
+        CGREEN = '\33[32m'
+        CYELLOW = '\33[33m'
+        CEND = '\033[0m'
+    else:
+        CRED = ''
+        CGREEN = ''
+        CYELLOW = ''
+        CEND = ''
+
+    if 'diff' not in res:
+        result += CYELLOW + 'diff: n/a\n' + CEND + "\n"
+        return result
+
+    diffs = []
+    if isinstance(res['diff'],list):
+        for diff in res['diff']:
+            before = diff.get('before', '')
+            after = diff.get('after', '')
+
+            after = after.splitlines(keepends=True)
+            before = before.splitlines(keepends=True)
+
+            if 'before_header' in diff:
+                fromfile = diff['before_header']
+            else:
+                fromfile = '/dev/null'
+            tofile = diff['after_header']
+            diffs.extend(difflib.unified_diff(before, after,
+                fromfile=fromfile, tofile=tofile))
+
+        for line in diffs:
+            if not line.endswith('\n'):
+                line += '\n'
+
+            if line.startswith('-'):
+                result += CRED + line + CEND
+            elif line.startswith('+'):
+                result += CGREEN + line + CEND
+            else:
+                result += line
+
+        result += "\n"
+    elif isinstance(res['diff'], dict):
+        diff = res['diff']
+        before = diff.get('before', '')
+        after = diff.get('after', '')
+
+        if type(before) is not str:
+            result += CRED +   '--- BEFORE: ' + str(before) + CEND + "\n"
+            result += CGREEN + '+++  AFTER: ' + str(after) + CEND + "\n"
+            result += "\n"
+            return result
+
+        after = after.splitlines(keepends=True)
+        before = before.splitlines(keepends=True)
+
+        if 'before_header' in diff:
+            fromfile = diff['before_header']
+        else:
+            fromfile = '/dev/null'
+        if 'after_header' in diff:
+            tofile = diff['after_header']
+        else:
+            tofile = '/dev/null'
+        diffs.extend(difflib.unified_diff(before, after,
+            fromfile=fromfile, tofile=tofile))
+
+        for line in diffs:
+            if not line.endswith('\n'):
+                line += '\n'
+
+            if line.startswith('-'):
+                result += CRED + line + CEND
+            elif line.startswith('+'):
+                result += CGREEN + line + CEND
+            else:
+                result += line
+        result += "\n"
+    else:
+        result += CRED + 'Unknown format for diff: ' + type(res['diff']) + CEND + "\n"
+
+    return result
+
 def read_play(filename, highlighting=None):
     result = ''
     NL = '\n'
@@ -49,48 +148,11 @@ def read_play(filename, highlighting=None):
 
                 result += f"[{hostname}]  {taskname}" + "\n"
 
-                if 'diff' not in host:
-                    result += CYELLOW + 'diff: n/a\n' + CEND + "\n"
-                    continue
-
-                diffs = []
-                if isinstance(host['diff'],list):
-                    for diff in host['diff']:
-                        before = diff.get('before', '')
-                        after = diff.get('after', '')
-
-                        after = after.splitlines(keepends=True)
-                        before = before.splitlines(keepends=True)
-
-                        if 'before_header' in diff:
-                            fromfile = diff['before_header']
-                        else:
-                            fromfile = '/dev/null'
-                        tofile = diff['after_header']
-                        diffs.extend(difflib.unified_diff(before, after,
-                            fromfile=fromfile, tofile=tofile))
-
-                    for line in diffs:
-                        if not line.endswith('\n'):
-                            line += '\n'
-
-                        if line.startswith('-'):
-                            result += CRED + line + CEND
-                        elif line.startswith('+'):
-                            result += CGREEN + line + CEND
-                        else:
-                            result += line
-
-                    result += "\n"
-                elif isinstance(host['diff'], dict):
-                    diff = host['diff']
-                    before = diff.get('before', '')
-                    after = diff.get('after', '')
-                    result += CRED +   '--- BEFORE: ' + str(before) + CEND + "\n"
-                    result += CGREEN + '+++  AFTER: ' + str(after) + CEND + "\n"
-                    result += "\n"
+                if 'results' in host:
+                    for res in host['results']:
+                        result += parse_result(res, highlighting)
                 else:
-                    result += CRED + 'Unknown format for diff: ' + type(host['diff']) + CEND + "\n"
+                    result += parse_result(host, highlighting)
 
     return (start + result + end).replace('\n', NL)
 
