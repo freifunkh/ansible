c065e751c0f4d164501e01a4e4e759144be13193
aiyion.prime <git@aiyionpri.me>
2021-09-14 12:35:08 +0200

    cli_tools: change url of ffh_chosen_gw to /api

--- roles/ffh.cli_tools/files/bin/ffh_chosen_gw.sh
+++ roles/ffh.cli_tools/files/bin/ffh_chosen_gw.sh
@@ -2,7 +2,7 @@
 
 # the addresses of one supernode do only differ in the 4th and last octet, so we match for the four octets
 node_id_prefix=$(cat /sys/class/net/bat10/address | cut -d ':' -f 1-3 | tr -d ':')..$(cat /sys/class/net/bat10/address | cut -d ':' -f 5 | tr -d ':')
-meshviewer=$(curl --compressed -s "https://harvester.ffh.zone/meshviewer.json" | jq '.nodes | map({ gateway: .gateway, gateway6: .gateway6})')
+meshviewer=$(curl --compressed -s "https://harvester.ffh.zone/api/meshviewer.json" | jq '.nodes | map({ gateway: .gateway, gateway6: .gateway6})')
 
 get() {
 	echo "$meshviewer" | jq 'map(select(.'$1' and (.'$1' | gsub(":";"") | match("^'${node_id_prefix}'")))) | length'
