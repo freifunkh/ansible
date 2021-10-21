e883dc5aff0b818bc8efecdfa48420a983f745ae
lemoer <git@irrelefant.net>
2021-10-22 01:21:14 +0200

    cli_tools: ffh_babel_stat.sh: show new ipv6 prefix

--- roles/ffh.cli_tools/files/bin/ffh_babel_stat.sh
+++ roles/ffh.cli_tools/files/bin/ffh_babel_stat.sh
@@ -25,11 +25,13 @@ ifaces=$(d | grep interface | cut -d " " -f 3)
 TOK_10="route 10\\." 
 TOK_100="route 100\\." 
 TOK_2a02="route 2a02:790:ff:"
+TOK_2001="route 2001:678:978:"
 
 xroute_10=$(d | grep "$TOK_10" | count_lines)
 xroute_100=$(d | grep "$TOK_100" | count_lines)
 xroute_2a02=$(d | grep "$TOK_2a02" | count_lines)
-xroute_rest=$(d | grep "xroute" | grep -v "$TOK_10" | grep -v "$TOK_2a02" | grep -v "$TOK_100" )
+xroute_2001=$(d | grep "$TOK_2001" | count_lines)
+xroute_rest=$(d | grep "xroute" | grep -v "$TOK_10" | grep -v "$TOK_2a02" | grep -v "$TOK_2001" | grep -v "$TOK_100" )
 xroute_rest_cnt=$(echo "$xroute_rest" | count_lines)
 
 echo
@@ -42,10 +44,11 @@ echo
 echo "EXPORTED ROUTES:"
 echo
 
-echo "ipv4 routes beginning with 10..           : $xroute_10"
-echo "ipv4 routes beginning with 100..          : $xroute_100"
-echo "ipv6 routes beginning with 2a02:790:ff:...: $xroute_2a02"
-echo "other routes                              : $xroute_rest_cnt"
+printf "ipv4 routes beginning with  10..           : %4d\n" $xroute_10
+printf "ipv4 routes beginning with 100..           : %4d\n" $xroute_100
+printf "ipv6 routes beginning with 2a02:790:ff:... : %4d\n" $xroute_2a02
+printf "ipv6 routes beginning with 2001:678:978:...: %4d\n" $xroute_2001
+printf "other routes                               : %4d\n" $xroute_rest_cnt
 echo
 if [ $xroute_rest_cnt -gt 0 ]; then
 	echo "other routes are:"
@@ -84,21 +87,23 @@ TOK_2a02="prefix 2a02:790:ff:"
 xroute_10=$(ifroutes | grep "$TOK_10" | count_lines)
 xroute_100=$(ifroutes | grep "$TOK_100" | count_lines)
 xroute_2a02=$(ifroutes | grep "$TOK_2a02" | count_lines)
-xroute_rest=$(ifroutes | grep -v "$TOK_10" | grep -v "$TOK_2a02" | grep -v "$TOK_100" )
+xroute_2001=$(ifroutes | grep "$TOK_2001" | count_lines)
+xroute_rest=$(ifroutes | grep "xroute" | grep -v "$TOK_10" | grep -v "$TOK_2a02" | grep -v "$TOK_2001" | grep -v "$TOK_100")
 xroute_rest_cnt=$(echo "$xroute_rest" | count_lines)
 
 fmt_route () {
 	cut -d " " -f 4-7,12-15,8,9
 }
 
-echo "inactive routes                                  : $(ifroutes_deact | count_lines)"
-echo "active routes                                    : $(ifroutes | count_lines)"
-echo "routes total                                     : $(ifroutes_total | count_lines)"
+printf "inactive routes                                   : %4d\n" $(ifroutes_deact | count_lines)
+printf "active routes                                     : %4d\n" $(ifroutes | count_lines)
+printf "routes total                                      : %4d\n" $(ifroutes_total | count_lines)
 echo
-echo "active ipv4 routes beginning with 10..           : $xroute_10"
-echo "active ipv4 routes beginning with 100..          : $xroute_100"
-echo "active ipv6 routes beginning with 2a02:790:ff:...: $xroute_2a02"
-echo "active other routes                              : $xroute_rest_cnt"
+printf "active ipv4 routes beginning with  10..           : %4d\n" $xroute_10
+printf "active ipv4 routes beginning with 100..           : %4d\n" $xroute_100
+printf "active ipv6 routes beginning with 2a02:790:ff:... : %4d\n" $xroute_2a02
+printf "active ipv6 routes beginning with 2001:678:978:...: %4d\n" $xroute_2001
+printf "active other routes                               : %4d\n" $xroute_rest_cnt
 echo
 if [ $xroute_rest_cnt -gt 0 ]; then
 	echo "other active routes are:"
