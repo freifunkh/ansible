72be870144dfcd844b55a81f1f3dc3e39de5c6db
Oliver Krueger <mail@oliverkrueger.de>
2021-11-25 23:02:21 +0100

    tests: add cron.sh, fix permissions

---
+++ tests/cron.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+export ANSIBLE_STDOUT_CALLBACK=json
+time=$(date -Iseconds)
+
+git fetch --all --quiet
+git reset --quiet --hard origin/master
+
+mkdir -p /tmp/auto/dailyrun
+
+ansible-playbook playbooks/supernodes.yml -CD --extra-vars "show_secret_diffs=no" > /tmp/auto/dailyrun/${time}.json
+python3 tests/mail.py /tmp/auto/dailyrun/${time}.json
