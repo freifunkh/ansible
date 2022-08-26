2504feb8007539a4156bfc38bdfe789893f48f5d
Oliver Krueger <mail@oliverkrueger.de>
2022-08-26 06:50:54 +0000

    mesh_wireguard: fix ffh_wg_peercounter.sh to output correct timestamps

--- roles/ffh.mesh_wireguard/files/bin/ffh_wg_peercounter.sh
+++ roles/ffh.mesh_wireguard/files/bin/ffh_wg_peercounter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wg show all dump | awk 'BEGIN {}; {if (systime()-$6 <180 ) print $1 } ; END {}' | uniq -c | awk 'BEGIN {}; { print "wireguard_device,name=" $2 ",type=linux_kernel established_peers=" $1 "i " systime() } ; END {}'
+wg show all dump | awk 'BEGIN {}; {if (systime()-$6 <180 ) print $1 } ; END {}' | uniq -c | awk 'BEGIN {}; { print "wireguard_device,name=" $2 ",type=linux_kernel established_peers=" $1 "i " (systime()*1e9) } ; END {}'
