7f832680d31f0772874c5dc8dd40a391be2fe2f3
lemoer <git@irrelefant.net>
2022-12-09 00:08:43 +0100

    mesh_wireguard: add ufu counters

--- roles/ffh.mesh_wireguard/files/bin/ffh_wg_peercounter.sh
+++ roles/ffh.mesh_wireguard/files/bin/ffh_wg_peercounter.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
-wg show all dump | awk 'BEGIN {}; {if (systime()-$6 <180 ) print $1 } ; END {}' | uniq -c | awk 'BEGIN {}; { print "wireguard_device,name=" $2 ",type=linux_kernel established_peers=" $1 "i " (systime()*1e9) } ; END {}'
+wg show all dump | awk '
+BEGIN {
+	i = 1;
+	while ( "cat /etc/wireguard/peers-wg/ufu-*" | getline a > 0 ) {
+		i=i+1;
+		ufu[i]=a;
+	}
+	t = systime();
+};
+{
+	if ((t-$6)<180) {
+		for (key in ufu) {
+			if (ufu[key] == $2) {
+				print $1 " ufu";
+			}
+		}
+		print $1 " all"
+	}
+};
+END {}' | sort | uniq -c | awk '
+BEGIN {};
+{
+    print "wireguard_device,name=" $2 ",type=linux_kernel,group=" $3 " established_peers=" $1 "i " (systime()*1e9)
+};
+END {}'
