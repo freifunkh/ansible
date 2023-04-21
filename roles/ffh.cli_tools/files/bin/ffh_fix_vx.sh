3921ee2b5f4f96ab3994d43ac3c63333bb5b8c05
Oliver Krueger <mail@oliverkrueger.de>
2023-04-21 10:07:00 +0000

    cli_tools: remove ffh_fix_vx.sh

--- roles/ffh.cli_tools/files/bin/ffh_fix_vx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for I in `seq 10 23` 99; do
-  /usr/local/sbin/batctl meshif bat$I if add vx-$I
-done;
-
-for I in 0 `seq 10 23` 99; do
-  /usr/local/sbin/batctl meshif bat$I if add vlan-gt-2$I
-done;
