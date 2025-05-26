162eac6d8aba856250ae4600c01f688dc66f2bab
Oliver Krueger <freifunk@oliverkrueger.de>
2025-05-26 02:03:49 +0200

    rdns: remove host

--- tests/cron.sh
+++ tests/cron.sh
@@ -8,7 +8,7 @@ git reset --quiet --hard origin/master
 
 mkdir -p /tmp/auto/dailyrun
 
-for machine in supernodes superexitnodes exitnodes proxmoxes dns ns1 mx1 db tonne harvester vds web rdns buildclouds evergiven dali thetisd rena
+for machine in supernodes superexitnodes exitnodes proxmoxes dns ns1 mx1 db tonne harvester vds web buildclouds evergiven dali thetisd rena
 do
   ansible-playbook playbooks/${machine}.yml -CD --extra-vars "show_secret_diffs=no" > /tmp/auto/dailyrun/${time}-${machine}.json
   python3 tests/mail.py /tmp/auto/dailyrun/${time}-${machine}.json
